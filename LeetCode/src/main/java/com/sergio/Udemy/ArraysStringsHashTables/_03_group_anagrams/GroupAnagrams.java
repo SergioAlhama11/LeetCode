6beb75861588316ef5a70cade2beae3e6370bcbd
Sergio Alhama <sergio.alhama.11@gmail.com>
2025-12-10 13:10:59 +0100

    [ArraysStringsHashTables]: Implementing GroupAnagrams exercise solution

--- LeetCode/src/main/java/com/sergio/Udemy/ArraysStringsHashTables/_03_group_anagrams/GroupAnagrams.java
+++ LeetCode/src/main/java/com/sergio/Udemy/ArraysStringsHashTables/_03_group_anagrams/GroupAnagrams.java
@@ -1,4 +1,43 @@
 package com.sergio.Udemy.ArraysStringsHashTables._03_group_anagrams;
 
+
+import java.util.*;
+
+/**
+ * Un anagrama es una palabra creada a partir de la reordenacion de las letras de otra palabra. Ej: saco - caso
+ * Dado un array de strings, devuelve los anagramas agrupados, Cualquier orden es valido.
+ *
+ * Ejemplo:
+ *  Input: words = ["saco", "arresto", "programa", "rastreo", "caso"].
+ *  Output: [["saco", "caso"], ["arresto", "rastreo"], ["programa"]].
+ */
 public class GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) return Collections.emptyList();
+
+        Map<String, List<String>> anagramMap = buildAnagramMap(strs);
+        return new ArrayList<>(anagramMap.values());
+    }
+
+    private Map<String, List<String>> buildAnagramMap(String[] strs) {
+        Map<String, List<String>> anagramMap = new HashMap<>();
+        for (String s : strs) {
+            String hash = getAnagramHash(s);
+            if (!anagramMap.containsKey(hash)) {
+               anagramMap.put(hash, new ArrayList<>());
+            }
+            anagramMap.get(hash).add(s);
+        }
+        return anagramMap;
+    }
+
+    private String getAnagramHash(String s) {
+        int[] letterCount = new int[26];
+        for (int c : s.toCharArray()) {
+            letterCount[c - 'a']++;
+        }
+
+        return Arrays.toString(letterCount);
+    }
 }
