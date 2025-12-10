757600659bfe5556cb0350a4750324654d02073a
Sergio Alhama <sergio.alhama.11@gmail.com>
2025-12-10 14:35:10 +0100

    [LinkedLists]: Implementing RemoveDups exercise solution

--- LeetCode/src/main/java/com/sergio/Udemy/LinkedLists/_01_remove_dups/RemoveDups.java
+++ LeetCode/src/main/java/com/sergio/Udemy/LinkedLists/_01_remove_dups/RemoveDups.java
@@ -1,4 +1,30 @@
 package com.sergio.Udemy.LinkedLists._01_remove_dups;
 
+import com.sergio.Udemy.LinkedLists._00_linkedList.Node;
+
+import java.util.HashSet;
+
+/**
+ * Escribe un algoritmo para eliminar los elementos duplicados en una Lista enlazada
+ *
+ * Ejemplo:
+ *  Input: 1 -> 2 -> 2 -> 3 -> 4 -> 1
+ *  Output: 1 -> 2 -> 3 -> 4
+ */
 public class RemoveDups {
+
+    public void removeDups(Node head) {
+        HashSet<Integer> found = new HashSet<>();
+        found.add(head.value);
+
+        Node current = head;
+        while (current != null && current.next != null) {
+            if (!found.add(current.next.value)) {
+                // Si no podemos añadir el siguiente elemento es porque ya existe, por lo que debemos eliminarlo
+                current.next = current.next.next;
+            }
+            // Avanzamos el puntero
+            current = current.next;
+        }
+    }
 }
