5b13be609a8beeb4c2e2aad14e78c6bdf703db4a
Sergio Alhama <sergio.alhama.11@gmail.com>
2025-12-10 14:35:55 +0100

    [LinkedLists]: Implementing NthNodeToLast exercise solution

--- LeetCode/src/main/java/com/sergio/Udemy/LinkedLists/_03_nth_node_to_last/NthNodeToLast.java
+++ LeetCode/src/main/java/com/sergio/Udemy/LinkedLists/_03_nth_node_to_last/NthNodeToLast.java
@@ -1,4 +1,33 @@
 package com.sergio.Udemy.LinkedLists._03_nth_node_to_last;
 
+import com.sergio.Udemy.LinkedLists._00_linkedList.Node;
+
+/**
+ * Dada una lista enlazada simple y un valor N, devuelve el nodo N empezando por el final
+ *
+ * Ejemplo:
+ *  Input: 1 -> 2 -> 4 -> 6 -> NULL|| n: 1
+ *  Output: 4
+ */
 public class NthNodeToLast {
+
+    // Utilizando dos punteros, si primero avanzamos n posiciones el primero, y a continuacion
+    // avanzamos ambos al mismo ritmo, cuando el primero llegue al final de la lista, el segundo
+    // estara en la posicion deseada.
+    public Node nthNodeToLast(Node head, int n) {
+        Node p1 = head;
+        Node p2 = head;
+
+        for (int i = 0; i < n; i++) {
+            if (p1 == null) return null;
+            p1 = p1.next;
+        }
+
+        while (p1 != null) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        return p2;
+    }
 }
