0168f286a645c29d2fff26120218ee4a7ace84c3
Sergio Alhama <sergio.alhama.11@gmail.com>
2025-12-10 11:40:56 +0100

    [ArraysStringsHashTables]: Restablish commit

--- src/main/java/com/sergio/leetCode/Udemy/ArraysStringsHashTables/_01_is_unique/IsUnique.java
+++ src/main/java/com/sergio/leetCode/Udemy/ArraysStringsHashTables/_01_is_unique/IsUnique.java
@@ -4,13 +4,15 @@ package com.sergio.leetCode.Udemy.ArraysStringsHashTables._01_is_unique;
 import java.util.HashSet;
 import java.util.Set;
 
-/*  Dado un método que recibe un String, comprobar si todos los caracteres son únicos o no.
-
-    isUnique("abcde") => true;
-    isUnique("abcded") => false;ssssss
+/**
+ * Dado un metodo que recibe un String, comprobar si todos los caracteres son unicos o no.
+ *
+ * isUnique("abcde") => true;
+ * isUnique("abcded") => false;
  */
 public class IsUnique {
 
+    // Asumiendo que es ASCII. Dependiendo de la codificacion seran mas
     private static int NUMBER_OF_CHARS = 128;
 
     public boolean isUnique(String s) {
