a0f562ae8801964da56ee7bb74df882bb39c22e5
Sergio Alhama <sergio.alhama.11@gmail.com>
2025-12-15 10:32:33 +0100

    [Trees]: Implementing InvertBinaryTree exercise solution + Unit Tests

---
+++ src/test/java/com/sergio/Udemy/Trees/_01_invert_binary_tree/InvertBinaryTreeTest.java
@@ -0,0 +1,11 @@
+package com.sergio.Udemy.Trees._01_invert_binary_tree;
+
+import org.junit.jupiter.api.Test;
+
+public class InvertBinaryTreeTest {
+
+    @Test
+    void invertBinaryTreeTest() {
+
+    }
+}
