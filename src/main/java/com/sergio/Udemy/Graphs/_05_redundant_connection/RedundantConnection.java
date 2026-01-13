51b35f2e48f96c96c866f8e5a385afa715d1dc52
Sergio Alhama <sergio.alhama.11@gmail.com>
2026-01-13 15:08:48 +0100

    [Graphs]: Implementing RedundantConnection exercise solution + Unit Tests

---
+++ src/main/java/com/sergio/Udemy/Graphs/_05_redundant_connection/RedundantConnection.java
@@ -0,0 +1,50 @@
+package com.sergio.Udemy.Graphs._05_redundant_connection;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/*
+ * Dado un grafo formado tras añadir una arista entre dos nodos de un árbol, devuelve
+ * la arista que puede ser eliminada para volver a formar un árbol.
+ *
+ * En caso de existir múltiples soluciones, devuelve la última arista del input.
+ *
+ * Ejemplo 1:
+ *  Input: [[1,2],[1,3],[2,3]]
+ *  Output: [2,3]
+ *
+ * Ejemplo 2:
+ *  Input: [[1,2],[2,3],[3,4],[1,4],[1,5]]
+ *  Output: [1,4]
+ */
+public class RedundantConnection {
+
+  public int[] findRedundantConnection(int[][] edges) {
+    List<Set<Integer>> adjList = new ArrayList<>();
+    for (int i = 0; i < 1000; i++) {
+      adjList.add(new HashSet<>());
+    }
+
+    for (int[] edge : edges) {
+      int first = edge[0];
+      int second = edge[1];
+      if (dfs(first, second, -1, adjList)) return edge;
+      adjList.get(first).add(second);
+      adjList.get(second).add(first);
+    }
+
+    return null;
+  }
+
+  private boolean dfs(int first, int second, int previous, List<Set<Integer>> adjList) {
+    if (first == second) return true;
+
+    for (int other : adjList.get(first)) {
+      if (other == previous) continue;
+      if (dfs(other, second, first, adjList)) return true;
+    }
+    return false;
+  }
+}
