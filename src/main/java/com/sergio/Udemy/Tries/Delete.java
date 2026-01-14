10fb1b8f01bf51f093c5206b671c6f2b551423dd
Sergio Alhama <salhama@indra.es>
2026-01-14 11:03:38 +0100

    [Tries]: Implementing Trie exercise solution + Unit Tests

--- src/main/java/com/sergio/Udemy/Tries/Delete.java
+++
@@ -1,4 +0,0 @@
-package com.sergio.Udemy.Tries;
-
-public class Delete {
-}
---
+++ src/main/java/com/sergio/Udemy/Tries/_00_trie/Trie.java
@@ -0,0 +1,40 @@
+package com.sergio.Udemy.Tries._00_trie;
+
+public class Trie {
+  TrieNode root;
+
+  public Trie() {
+    root = new TrieNode();
+  }
+
+  public void insert(String word) {
+    TrieNode currentNode = root;
+    String lowerCaseWord = word.toLowerCase();
+
+    for (char c : lowerCaseWord.toCharArray()) {
+      TrieNode child = currentNode.children.get(c);
+
+      if (child == null) {
+        child = new TrieNode();
+        currentNode.children.put(c, child);
+      }
+      currentNode = child;
+    }
+    currentNode.isEndOfWord = true;
+  }
+
+  // M E S A
+  // Prefix: ME -> me // [m, e]
+
+  public boolean search(String prefix) {
+    TrieNode currentNode = root;
+    String prefixLowercase = prefix.toLowerCase();
+
+    for (char c : prefixLowercase.toCharArray()) {
+      currentNode = currentNode.children.get(c);
+
+      if (currentNode == null) return false;
+    }
+    return true;
+  }
+}
