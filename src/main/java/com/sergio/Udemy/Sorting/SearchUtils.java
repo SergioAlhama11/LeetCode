c5ffd52e78c20820c6a617557d9fd305a765e0f0
Sergio Alhama <salhama@indra.es>
2026-01-15 13:05:50 +0100

    [Sorting]: Implementing MergeSort exercise solution unit tests +

---
+++ src/main/java/com/sergio/Udemy/Sorting/SearchUtils.java
@@ -0,0 +1,10 @@
+package com.sergio.Udemy.Sorting;
+
+public class SearchUtils {
+
+  public static void swap(int[] array, int i, int j) {
+    int temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
