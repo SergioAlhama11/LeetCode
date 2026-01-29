9a190b363568bd960aa1345c436a573a11c6dd21
Sergio Alhama <salhama@indra.es>
2026-01-29 18:54:58 +0100

    [ExtraProblems]: Implementing HouseRobber exercise solution + Unit Tests

--- src/test/java/com/sergio/Udemy/ExtraProblems/_05_house_robber/HouseRobberTest.java
+++ src/test/java/com/sergio/Udemy/ExtraProblems/_05_house_robber/HouseRobberTest.java
@@ -1,3 +1,33 @@
 package com.sergio.Udemy.ExtraProblems._05_house_robber;
 
-public class HouseRobberTest {}
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class HouseRobberTest {
+  private final HouseRobber solver = new HouseRobber();
+
+  @Test
+  void houseRobberTest1() {
+    int[] arr = {1,2,3,1};
+    assertEquals(4, solver.rob(arr));
+  }
+
+  @Test
+  void houseRobberDPTest1() {
+    int[] arr = {1,2,3,1};
+    assertEquals(4, solver.dpRob(arr));
+  }
+
+  @Test
+  void houseRobberTest2() {
+    int[] arr = {2,7,9,3,1};
+    assertEquals(12, solver.rob(arr));
+  }
+
+  @Test
+  void houseRobberDPTest2() {
+    int[] arr = {2,7,9,3,1};
+    assertEquals(12, solver.dpRob(arr));
+  }
+}
