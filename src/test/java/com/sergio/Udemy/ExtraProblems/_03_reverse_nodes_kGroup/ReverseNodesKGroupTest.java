e75811ead30a3ea6628942b7c34140677ba62da2
Sergio Alhama <salhama@indra.es>
2026-01-29 18:24:25 +0100

    [ExtraProblems]: Implementing ReverseNodes exercise solution + Unit Tests

---
+++ src/test/java/com/sergio/Udemy/ExtraProblems/_03_reverse_nodes_kGroup/ReverseNodesKGroupTest.java
@@ -0,0 +1,34 @@
+package com.sergio.Udemy.ExtraProblems._03_reverse_nodes_kGroup;
+
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.sergio.Udemy.ExtraProblems._03_reverse_nodes_kgroup.ReverseNodesKGroup;
+import org.junit.jupiter.api.Test;
+
+public class ReverseNodesKGroupTest {
+
+  @Test
+  public void reverseNodesKGroupTest() {
+    ReverseNodesKGroup reverse = new ReverseNodesKGroup();
+
+    ReverseNodesKGroup.ListNode node = reverse.new ListNode(1);
+    node.next = reverse.new ListNode(2);
+    node.next.next = reverse.new ListNode(3);
+    node.next.next.next = reverse.new ListNode(4);
+    node.next.next.next.next = reverse.new ListNode(5);
+    node.next.next.next.next.next = reverse.new ListNode(6);
+    node.next.next.next.next.next.next = reverse.new ListNode(7);
+
+    ReverseNodesKGroup.ListNode newHead = reverse.reverseKGroup(node, 4);
+
+    assertEquals(4, newHead.val);
+    assertEquals(3, newHead.next.val);
+    assertEquals(2, newHead.next.next.val);
+    assertEquals(1, newHead.next.next.next.val);
+    assertEquals(5, newHead.next.next.next.next.val);
+    assertEquals(6, newHead.next.next.next.next.next.val);
+    assertEquals(7, newHead.next.next.next.next.next.next.val);
+  }
+}
